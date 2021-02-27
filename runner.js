b0403eed7d8ac18814b09d0757a9a29420768627
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2021-02-27 15:21:19 +0530

    ignoring test files in node modules

--- runner.js
+++ runner.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 
+const forbiddenDirs = ['node_modules'];
 class Runner {
     constructor() {
         this.testFiles = [];
@@ -44,7 +45,7 @@ class Runner {
                     name: filePath,
                     shortName: file
                 });
-            } else if (stats.isDirectory()) {
+            } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
                 const childFiles = await fs.promises.readdir(filePath);
                 files.push(...childFiles.map(f => path.join(file, f)));
             }
