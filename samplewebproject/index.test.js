28a32f73e0cc2e19b3b8eb720dca225f70c9911e
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2021-02-27 19:45:48 +0530

    waiting for all the scripts to load

--- samplewebproject/index.test.js
+++ samplewebproject/index.test.js
@@ -4,4 +4,28 @@ it('has a text input', async ()=>{
     const dom = await render('index.html');
     const input = dom.window.document.querySelector('input');
     assert(input);
-})
+});
+
+it('shows a success message with a valid email', async ()=>{
+    const dom = await render('index.html');
+    const input = dom.window.document.querySelector('input');
+    input.value = "fadfa@dsfdfco";
+
+    const form = dom.window.document.querySelector('form');
+    form.dispatchEvent(new dom.window.Event('submit'));
+
+    const h1 = dom.window.document.querySelector('h1');
+    assert.strictEqual(h1.textContent, 'Looks Good');
+});
+
+it('shows a fail message with a invalid email', async ()=>{
+    const dom = await render('index.html');
+    const input = dom.window.document.querySelector('input');
+    input.value = "fadfadsfdfco";
+
+    const form = dom.window.document.querySelector('form');
+    form.dispatchEvent(new dom.window.Event('submit'));
+
+    const h1 = dom.window.document.querySelector('h1');
+    assert.strictEqual(h1.textContent, 'Invalid Email');
+});
